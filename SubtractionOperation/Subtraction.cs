2740997979c60e3830c90b8bf1d88542a27f6ca6
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-13 10:31:23 +0200

    Feature: Added subtraction and square root operations

---
+++ SubtractionOperation/Subtraction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SubtractionOperation
+{
+    public class Subtraction
+    {
+        public static int Difference(int a, int b)
+        {
+            return a - b;
+        }
+        public static double Difference(double a, double b)
+        {
+            return a - b;
+        }
+        public static int Difference(int[] numbers)
+        {
+            int c = 0;
+
+            foreach(int a in numbers)
+            {
+                c = Difference(c, a);
+            }
+            return c;
+        }
+        public static double Difference(double[] numbers)
+        {
+            double c = 0.0;
+
+            foreach(double a in numbers)
+            {
+                c = Difference(c, a);
+            }
+            return c;
+
+        }
+
+    }
+}
