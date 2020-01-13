9830d87036fef42ea92ca74ff3663011de31ae41
enockC <58557480+enockC@users.noreply.github.com>
2020-01-13 14:40:09 +0200

    Feature: Addition

---
+++ Addition/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Addition
+{
+    public static class Addition
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Sum(double[] arrayA)
+        {
+            double c = 0.0;
+            foreach (double a in arrayA)
+            {
+                c = Sum(a, c);
+            }
+
+            return c;
+        }
+
+        public static int Sum(int[] arrayA)
+        {
+            int c = 0;
+            foreach (int a in arrayA)
+            {
+                c = Sum(a, c);
+            }
+
+            return c;
+        }
+    }
+}
