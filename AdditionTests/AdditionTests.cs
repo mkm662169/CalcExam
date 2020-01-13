85de167fe3ac37a4866bcaf7a4dc1c8827be4e40
enockC <58557480+enockC@users.noreply.github.com>
2020-01-13 16:15:27 +0200

    Feature Edited add & Divide

--- AdditionTests/AdditionTests.cs
+++ AdditionTests/AdditionTests.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Addition;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace Addition
+namespace Addition.Tests
 {
     public static class Addition
     {
---
+++ DivisionTests/DivisionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Division;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Division.Tests
+{
+    [TestClass()]
+    public class DivisionTests
+    {
+        private readonly int a = 20;
+        private readonly int b = 10;
+        private readonly double c = 20.3;
+        private readonly double d = 10.2;
+        private readonly int e = 0;
+
+        [TestMethod()]
+        public void QuotientTest()
+        {
+            Assert.AreEqual(2, Division.Quotient(a, b));
+        }
+
+        [TestMethod()]
+        public void QuotientDoulbleTest()
+        {
+            Assert.AreEqual(1.9901960784313728, Division.Quotient(c, d));
+        }
+
+        [TestMethod()]
+        public void QuotientintQuotientZeroTest()
+        {
+            Assert.ThrowsException<DivideByZeroException>(() => Division.Quotient(a, e));
+        }
+    }
+}
+
