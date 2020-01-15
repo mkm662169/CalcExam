b87c7d015d218c4bb346c1ffbe59e8ca7d412ef4
enockC <58557480+enockC@users.noreply.github.com>
2020-01-15 09:57:34 +0200

    Feature: Mode Added

---
+++ Mode/Mode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Mode
+{
+    public class Mode
+    {
+
+        public static double Findmode(int[] x)
+        {
+            int high = x.OrderByDescending(n => n).First();
+            int mode = x.GroupBy(i => i)  //Grouping same items
+            .OrderByDescending(g => g.Count()) //now getting frequency of a value
+            .Select(g => g.Key) //selecting key of the group
+            .FirstOrDefault();   //Finally, taking the most frequent value
+
+            return mode;
+        }
+
+
+
+    }
+}
+
