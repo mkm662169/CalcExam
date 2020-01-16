b67f8db8c2f34d52aec67e6455e6ebcaecc88b4f
Jikamo <56824758+Jikamo@users.noreply.github.com>
2020-01-16 14:34:49 +0200

    Standard Deviation test added

---
+++ StandarddivTests1/StandardDevtest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatisticalCalculatorOperations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticalCalculatorOperations.Tests
+{
+    [TestClass()]
+    public class StandardDeviationTests
+    {
+        private readonly double[] Arraya = { 1, 2, 3, 4, 5, 6 };
+        [TestMethod()]
+        public void SDTest()
+        {
+            Assert.AreEqual(1.78, StandardDeviation.SD(Arraya));
+        }
+
+    }
+}
